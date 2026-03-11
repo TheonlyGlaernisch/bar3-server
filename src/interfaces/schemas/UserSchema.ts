befbace10f92bbbdc86865da740393bcb486ba42
TheonlyGlaernisch <glaernisch.gaming@gmail.com>
2026-03-11 17:06:25 +0100

    Define UserSchema with IUser interface and schema

--- src/interfaces/schemas/UserSchema.ts
+++ src/interfaces/schemas/UserSchema.ts
@@ -1,1 +1,35 @@
+import mongoose, { Schema, Document } from 'mongoose';
 
+export interface IUser extends Document {
+  _id: mongoose.Types.ObjectId;
+  name: string;
+  apiKeyHash: string;
+  createdAt: Date;
+  lastUsedAt: Date;
+}
+
+const userSchema = new Schema<IUser>(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: false,
+    },
+    apiKeyHash: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true,
+    },
+    lastUsedAt: {
+      type: Date,
+      default: new Date(),
+    },
+  },
+  {
+    timestamps: { createdAt: true, updatedAt: false },
+    collection: 'users',
+  }
+);
+
+export const User = mongoose.model<IUser>('User', userSchema);
