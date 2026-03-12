7b7cd71053d1363b5d01c1f77da7203a9fe2d6e2
someoneumayknow1 <stefan.tufler@stud.ken.ch>
2026-03-12 14:16:08 +0100

    Create campaignAnalytics.ts

---
+++ src/models/campaignAnalytics.ts
@@ -0,0 +1,47 @@
+import mongoose, { Document, Schema } from 'mongoose';
+import { CampaignAnalytics, Link, Pixel } from '../interfaces/analytics';
+
+type PixelDocument = Pixel;
+type LinkDocument = Link;
+type CampaignAnalyticsDocument = Document & Omit<CampaignAnalytics, '_id'>;
+
+const pixelSchema = new Schema<PixelDocument>(
+  {
+    id: { type: String, required: true },
+    auth: { type: String, required: true },
+    readCount: { type: Number, required: true, default: 0 },
+    readHistory: { type: [Number], required: true, default: [] },
+  },
+  { _id: false }
+);
+
+const linkSchema = new Schema<LinkDocument>(
+  {
+    url: { type: String, required: true },
+    id: { type: String, required: true },
+    auth: { type: String, required: true },
+    readCount: { type: Number, required: true, default: 0 },
+    readHistory: { type: [Number], required: true, default: [] },
+  },
+  { _id: false }
+);
+
+const campaignAnalyticsSchema = new Schema<CampaignAnalyticsDocument>(
+  {
+    name: { type: String, required: true, unique: true, index: true },
+    sentCount: { type: Number, required: true, default: 0 },
+    createdTime: { type: Number, required: true, index: true },
+    links: { type: [linkSchema], required: true, default: [] },
+    messagePixel: { type: pixelSchema, required: true },
+  },
+  {
+    collection: 'campaign_analytics',
+  }
+);
+
+const CampaignAnalyticsModel = mongoose.model<CampaignAnalyticsDocument>(
+  'CampaignAnalytics',
+  campaignAnalyticsSchema
+);
+
+export default CampaignAnalyticsModel;
