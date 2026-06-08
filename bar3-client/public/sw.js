193032cac19edebb0c68da65272c18767a23ee40
Stefan Tufler <stefan.tufler@stud.ken.ch>
2026-06-08 21:08:57 +0200

    Update sw.js

--- bar3-client/public/sw.js
+++ bar3-client/public/sw.js
@@ -1,1 +1,13 @@
+self.addEventListener('push', (event) => {
+  const data = event.data ? event.data.json() : {};
+  self.registration.showNotification(data.title || 'Bar 3', {
+    body: data.body || '',
+    icon: '/favicon.ico',
+    tag: data.tag || 'bar3-mention',
+  });
+});
 
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+  event.waitUntil(clients.openWindow('/chat'));
+});
